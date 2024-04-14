c19df08a9e7e2601e713076335dde54292267edd
= <=>
2024-04-14 21:37:42 +0100

    feat:basic menu done

--- src/app/views/menu/menu.component.ts
+++ src/app/views/menu/menu.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule
+  ],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  date = new FormControl(new Date());
+  activity = new FormControl('');
+  activities: { date: string; activity: string }[] = [];
 
+  saveActivity() {
+    if (this.date.value && this.activity.value) {
+      const newActivity = {
+        date: this.date.value.toLocaleDateString(), // Convertir la fecha a string
+        activity: this.activity.value
+      };
+      this.activities.push(newActivity);
+      this.activity.reset(); // Opcional: resetear el campo de actividad
+    }
+  }
 }
