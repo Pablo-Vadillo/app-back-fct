9e4e051f55a7263d0c29af38dfe92810c9dcaa08
= <=>
2024-04-14 21:05:53 +0100

    feat:Register basic structure done

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
 
+  constructor(private router: Router) {}
+
+  onRegister() {
+    if (this.registerForm.valid && this.registerForm.value.password === this.registerForm.value.confirmPassword) {
+      
+
+      this.router.navigate(['/menu']);
+    } else {
+      console.log('Form is not valid or passwords do not match');
+    }
+  }
 }
